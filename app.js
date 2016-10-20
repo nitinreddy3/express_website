aa1ab4892c606a62aab7864ef4d8652c9cb61cd5
Nitin Reddy <reddynitin3@gmail.com>
2016-10-20 07:49:32 +0530

    Added compression for best performance

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -21,6 +22,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
